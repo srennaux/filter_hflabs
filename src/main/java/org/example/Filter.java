c1578ea5e4ad478847323721f05587d1611d4ea8
srennaux <162683057+srennaux@users.noreply.github.com>
2024-03-19 18:19:04 +0300

    Update Filter.java

--- src/main/java/org/example/Filter.java
+++ src/main/java/org/example/Filter.java
@@ -4,18 +4,18 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Filter {
-    private final String regexp;          // private означает, что мы не можем узнать содержимое regexp в других классах
-    private final String replacement;   // final - не можем менять значение regex
+    private final String regexp;          
+    private final String replacement;   
 
-    public Filter(String regexp, String replacement) {      // constructor выполняется ТОЛЬКО один раз
+    public Filter(String regexp, String replacement) {      
         this.regexp = regexp;
         this.replacement = replacement;
     }
 
     public String filter(String address) {
-        Pattern pattern = Pattern.compile(regexp);      // pattern запоминает регулярное выражение для последующей работы с ним
-        Matcher matcher = pattern.matcher(address);     // pattern объединяет регулярное выражение с адресом к которому мы хотим его применить
-        if (matcher.find() /* true если регулярное выражение в адресе есть */ && /* и */ matcher.group(1).endsWith(matcher.group(3))) {
+        Pattern pattern = Pattern.compile(regexp);      
+        Matcher matcher = pattern.matcher(address);     
+        if (matcher.find() && matcher.group(1).endsWith(matcher.group(3))) {
             address = matcher.replaceFirst(replacement);
         }
         return address;
